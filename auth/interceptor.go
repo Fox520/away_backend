00dcee06ae7330bc734d3d41f18dd764f5f8eb09
Thomas <36572168+Fox520@users.noreply.github.com>
2021-10-03 08:01:45 +0200

    Skip Firebase/Redis by retrieving email from token

--- auth/interceptor.go
+++ auth/interceptor.go
@@ -2,10 +2,8 @@ package auth
 
 import (
 	"context"
-	"sync"
-	"time"
+	"fmt"
 
-	"github.com/go-redis/redis/v8"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
@@ -36,71 +34,27 @@ func AuthInterceptor(ctx context.Context) (context.Context, error) {
 	}
 	token := tokenSlice[0]
 	// Uncomment for token verification to take place
-	_, err := FirebaseAuth.VerifyIDToken(context.Background(), token)
+	authToken, err := FirebaseAuth.VerifyIDToken(context.Background(), token)
 	if err != nil {
 		logger.Print("Token verification error: ", err)
 		return nil, status.Error(codes.Unauthenticated, err.Error())
 	}
-
-	// Extract user id and email from the Firebase token then add to context
-
-	uid, err := extractUserID(token)
-	if err != nil {
-		return nil, err
+	// identities => {"email":[name@example.com]}
+	emails := authToken.Firebase.Identities["email"]
+	email := ""
+	switch t := emails.(type) {
+	case []interface{}:
+		email = fmt.Sprint(t[0])
+	default:
+		return nil, status.Error(codes.Internal, "Email not found")
 	}
 
-	email, err := extractUserEmail(uid)
-	if err != nil {
-		return nil, err
-	}
 	payload := map[string]string{
 		ContextEmailKey: email,
-		ContextUIDKey:   uid,
+		ContextUIDKey:   authToken.UID,
 		ContextTokenKey: token,
 	}
 	newCtx := context.WithValue(ctx, ContextMetaDataKey, payload)
 
 	return newCtx, nil
-}
-
-func extractUserEmail(uid string) (string, error) {
-	// Try avoiding network roundtrip
-	mail, err := getRedisClient().Get(context.Background(), "email:"+uid).Result()
-	if err == nil {
-		return mail, nil
-	}
-	ur, err := FirebaseAuth.GetUser(context.Background(), uid)
-	if err != nil {
-		logger.Print("Cannot get user: ", err)
-		return "", status.Error(codes.Unauthenticated, err.Error())
-	}
-	getRedisClient().Set(context.Background(), "email:"+uid, ur.Email, 0)
-	return ur.Email, nil
-}
-
-func extractUserID(token string) (string, error) {
-
-	// https://firebase.google.com/docs/auth/admin/verify-id-tokens#go
-	at, err := FirebaseAuth.VerifyIDToken(context.Background(), token)
-	if err != nil {
-		return "", status.Error(codes.Unauthenticated, err.Error())
-	}
-	return at.UID, nil
-}
-
-var once sync.Once
-var redisClient *redis.Client
-
-func getRedisClient() *redis.Client {
-	once.Do(func() {
-		client := redis.NewClient(&redis.Options{
-			Addr:        "localhost:6379",
-			Password:    "",
-			DB:          0,
-			MaxRetries:  -1,
-			DialTimeout: 400 * time.Millisecond,
-		})
-		redisClient = client
-	})
-	return redisClient
 }
